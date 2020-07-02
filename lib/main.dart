013e5b81708ee2fc4a74d781de02d697e8fa30c2
siabreu <siabreu@mbp-de-simone.home>
2020-07-02 16:54:31 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:buscadorgifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:buscadorgifs/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
